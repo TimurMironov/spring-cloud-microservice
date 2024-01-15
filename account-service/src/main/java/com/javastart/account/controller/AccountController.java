6b07f4206f331acc81bd238c9ce135604b4206ed
TimurMironov <mironov.timur.99@mail.ru>
2024-01-15 14:24:32 +0300

    Add validation in account-service

--- account-service/src/main/java/com/javastart/account/controller/AccountController.java
+++ account-service/src/main/java/com/javastart/account/controller/AccountController.java
@@ -3,10 +3,14 @@ package com.javastart.account.controller;
 import com.javastart.account.dto.AccountRequestDTO;
 import com.javastart.account.dto.AccountResponseDTO;
 import com.javastart.account.entity.Account;
+import com.javastart.account.exception.AccountServiceException;
 import com.javastart.account.service.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,19 +24,26 @@ public class AccountController {
     }
 
     @GetMapping("/{id}")
-    public AccountResponseDTO getAccount(@PathVariable(name = "id") Long id){
+    public AccountResponseDTO getAccount(@PathVariable(name = "id") Long id) {
         return new AccountResponseDTO(accountService.getAccountByID(id));
     }
 
     @PostMapping("/")
-    public ResponseEntity<HttpStatus> saveAccount(@RequestBody AccountRequestDTO accountRequestDTO){
-        accountService.saveAccount(new Account(accountRequestDTO));
-        return ResponseEntity.ok(HttpStatus.OK);
+    public ResponseEntity<HttpStatus> saveAccount(@RequestBody @Valid AccountRequestDTO accountRequestDTO
+            , BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            String message = createExceptionMessage(bindingResult);
+            throw new AccountServiceException(message);
+        } else {
+            accountService.saveAccount(new Account(accountRequestDTO));
+            return ResponseEntity.ok(HttpStatus.OK);
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<HttpStatus> updateAccount(@RequestBody AccountRequestDTO accountRequestDTO,
-                                                    @PathVariable(name = "id") Long id){
+                                                    @PathVariable(name = "id") Long id) {
         Account account = new Account(accountRequestDTO);
         account.setId(id);
         accountService.updateAccount(account);
@@ -40,8 +51,20 @@ public class AccountController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteAccount(@PathVariable(name = "id") Long id){
+    public ResponseEntity<HttpStatus> deleteAccount(@PathVariable(name = "id") Long id) {
         accountService.deleteAccount(id);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private String createExceptionMessage(BindingResult bindingResult) {
+        StringBuilder message = new StringBuilder();
+
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            message.append(error.getField())
+                    .append(" - ")
+                    .append(error.getDefaultMessage())
+                    .append("; \n");
+        }
+        return message.toString();
     }
 }
--- account-service/src/main/java/com/javastart/account/dto/AccountRequestDTO.java
+++ account-service/src/main/java/com/javastart/account/dto/AccountRequestDTO.java
@@ -1,5 +1,7 @@
 package com.javastart.account.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,10 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AccountRequestDTO {
 
+    @Email(message = "Invalid mail format. Mail should have format like example@mail.com")
     private String email;
 
+    @Pattern(regexp = "^\\d{11}$", message = "Invalid number format")
     private String phone;
 
 
